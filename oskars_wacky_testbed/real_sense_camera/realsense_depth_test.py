eab4fd060ded1da2ccc60e1fee9ff8ffe1a3a935
Oskar Larsson <larsson.9238@gmail.com>
2023-03-09 11:52:13 +0100

    unshelving distance tracking

--- oskars_wacky_testbed/real_sense_camera/realsense_depth_test.py
+++ oskars_wacky_testbed/real_sense_camera/realsense_depth_test.py
@@ -1,19 +1,17 @@
+import cv2  # state of the art computer vision algorithms library
+import numpy as np  # fundamental package for scientific computing
+import matplotlib.pyplot as plt  # 2D plotting library producing publication quality figures
+import pyrealsense2 as rs  # Intel RealSense cross-platform open-source API
 
-import cv2                                # state of the art computer vision algorithms library
-import numpy as np                        # fundamental package for scientific computing
-import matplotlib.pyplot as plt           # 2D plotting library producing publication quality figures
-import pyrealsense2 as rs                 # Intel RealSense cross-platform open-source API
 print("Environment Ready")
-
-
 
 # Setup:
 # pipe = rs.pipeline()
 # cfg = rs.config()
 # cfg.enable_device_from_file("../object_detection.bag")
 # profile = pipe.start(cfg)
-pipe = rs.pipeline()
 config = rs.config()
+pipe = rs.pipeline(config)
 
 # Get device product line for setting a supporting resolution
 pipeline_wrapper = rs.pipeline_wrapper(pipe)
@@ -32,8 +30,8 @@ if not found_rgb:
 profile = pipe.start(config)
 # Skip 5 first frames to give the Auto-Exposure time to adjust
 for x in range(5):
-  pipe.wait_for_frames()
-  
+    pipe.wait_for_frames()
+
 # Store next frameset for later processing:
 frameset = pipe.wait_for_frames()
 color_frame = frameset.get_color_frame()
@@ -43,15 +41,10 @@ depth_frame = frameset.get_depth_frame()
 pipe.stop()
 print("Frames Captured")
 
-
-
 color = np.asanyarray(color_frame.get_data())
 plt.rcParams["axes.grid"] = False
 plt.rcParams['figure.figsize'] = [12, 6]
 plt.imshow(color)
-
-
-
 
 colorizer = rs.colorizer()
 colorized_depth = np.asanyarray(colorizer.colorize(depth_frame).get_data())
@@ -69,19 +62,17 @@ colorized_depth = np.asanyarray(colorizer.colorize(aligned_depth_frame).get_data
 images = np.hstack((color, colorized_depth))
 plt.imshow(images)
 
-
-
 # Standard OpenCV boilerplate for running the net:
 height, width = color.shape[:2]
 expected = 300
 aspect = width / height
 resized_image = cv2.resize(color, (round(expected * aspect), expected))
 crop_start = round(expected * (aspect - 1) / 2)
-crop_img = resized_image[0:expected, crop_start:crop_start+expected]
+crop_img = resized_image[0:expected, crop_start:crop_start + expected]
 
 net = cv2.dnn.readNetFromCaffe("../MobileNetSSD_deploy.prototxt", "../MobileNetSSD_deploy.caffemodel")
 inScaleFactor = 0.007843
-meanVal       = 127.53
+meanVal = 127.53
 classNames = ("background", "aeroplane", "bicycle", "bird", "boat",
               "bottle", "bus", "car", "cat", "chair",
               "cow", "diningtable", "dog", "horse",
@@ -92,44 +83,39 @@ blob = cv2.dnn.blobFromImage(crop_img, inScaleFactor, (expected, expected), mean
 net.setInput(blob, "data")
 detections = net.forward("detection_out")
 
-label = detections[0,0,0,1]
-conf  = detections[0,0,0,2]
-xmin  = detections[0,0,0,3]
-ymin  = detections[0,0,0,4]
-xmax  = detections[0,0,0,5]
-ymax  = detections[0,0,0,6]
+label = detections[0, 0, 0, 1]
+conf = detections[0, 0, 0, 2]
+xmin = detections[0, 0, 0, 3]
+ymin = detections[0, 0, 0, 4]
+xmax = detections[0, 0, 0, 5]
+ymax = detections[0, 0, 0, 6]
 
 className = classNames[int(label)]
 
-cv2.rectangle(crop_img, (int(xmin * expected), int(ymin * expected)), 
-             (int(xmax * expected), int(ymax * expected)), (255, 255, 255), 2)
-cv2.putText(crop_img, className, 
+cv2.rectangle(crop_img, (int(xmin * expected), int(ymin * expected)),
+              (int(xmax * expected), int(ymax * expected)), (255, 255, 255), 2)
+cv2.putText(crop_img, className,
             (int(xmin * expected), int(ymin * expected) - 5),
-            cv2.FONT_HERSHEY_COMPLEX, 0.5, (255,255,255))
+            cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255))
 
 plt.imshow(crop_img)
-
-
 
 scale = height / expected
 xmin_depth = int((xmin * expected + crop_start) * scale)
 ymin_depth = int((ymin * expected) * scale)
 xmax_depth = int((xmax * expected + crop_start) * scale)
 ymax_depth = int((ymax * expected) * scale)
-xmin_depth,ymin_depth,xmax_depth,ymax_depth
-cv2.rectangle(colorized_depth, (xmin_depth, ymin_depth), 
-             (xmax_depth, ymax_depth), (255, 255, 255), 2)
+# xmin_depth, ymin_depth, xmax_depth, ymax_depth
+cv2.rectangle(colorized_depth, (xmin_depth, ymin_depth),
+              (xmax_depth, ymax_depth), (255, 255, 255), 2)
 plt.imshow(colorized_depth)
-
-
 
 depth = np.asanyarray(aligned_depth_frame.get_data())
 # Crop depth data:
-depth = depth[xmin_depth:xmax_depth,ymin_depth:ymax_depth].astype(float)
+depth = depth[xmin_depth:xmax_depth, ymin_depth:ymax_depth].astype(float)
 
 # Get data scale from the device and convert to meters
 depth_scale = profile.get_device().first_depth_sensor().get_depth_scale()
 depth = depth * depth_scale
-dist,_,_,_ = cv2.mean(depth)
+dist, _, _, _ = cv2.mean(depth)
 print("Detected a {0} {1:.3} meters away.".format(className, dist))
-
