c0a3efb7935c4811037ef279b4358355d8f58ad9
ludde <ludde.llj@gmail.com>
2023-04-03 15:57:30 +0200

    added static panda hande to aruco transform to panda_hand_aruco.py

--- code/catkin_ws/src/external_calibration/src/params/panda_hand_aruco.py
+++ code/catkin_ws/src/external_calibration/src/params/panda_hand_aruco.py
@@ -1,23 +1,13 @@
+
+# Params for Panda Hand to attached ArUco Offset
+
 frame_name = "panda_hand"
 
-# # Offset left ArUco
-# left_name = "left_aruco"
-# left_x = 3.6
-# left_y = 6.1
-# left_z = 4.3
-#
-# # Offset right ArUco
-# right_name = "right_aruco"
-# right_x = 3.6
-# right_y = -6.1
-# right_z = 4.3
-
-
 # Offset left ArUco
-left_name = "left_aruco"
-left_x = 3.6
-left_y = 5.5
-left_z = 4
+left_name = "aruco_[0]"
+left_x = 0.036
+left_y = 0.055
+left_z = 0.04
 
 left_rotation_w = 0.5
 left_rotation_x = -0.5
@@ -25,10 +15,10 @@ left_rotation_y = 0.5
 left_rotation_z = -0.5
 
 # Offset right ArUco
-right_name = "right_aruco"
-right_x = 3.6
-right_y = -5.5
-right_z = 4
+right_name = "aruco_[1]"
+right_x = 0.036
+right_y = -0.055
+right_z = 0.04
 
 right_rotation_w = 0.5
 right_rotation_x = -0.5
